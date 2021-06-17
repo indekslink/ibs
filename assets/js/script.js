742f99a9beb25a268d57ad114cfd5c730447160f
riskiputraalamzah <aryainggil3@gmail.com>
2021-06-18 00:52:18 +0700

    Mengubah tampilan navabr

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,31 @@
 const navbar = document.querySelector(".my-nav");
 const containerNavbar = document.querySelector(".my-nav .container");
+const menus = document.querySelector(".my-nav ul.menu");
+
+const toggle = document.querySelector(".hamburger-menu");
+
+let currentScroll = 0;
 window.onscroll = function () {
-  const scroll = window.scrollY;
-  if (scroll > 50) {
-    navbar.style.height = containerNavbar.offsetHeight;
-    navbar.classList.add("scrolling");
-  } else {
-    navbar.classList.remove("scrolling");
+  let scroll = window.scrollY;
+
+  currentScroll = scroll;
+  // for navbar
+  navbar.style.height = containerNavbar.offsetHeight;
+  if (!toggle.classList.contains("show-menu")) {
+    navbar.classList.toggle("scrolling", scroll > 10);
   }
 };
+// setInterval(() => {
+//   console.log(currentScroll);
+// }, 1000);
+
+toggle.addEventListener("click", function () {
+  if (!navbar.classList.contains("scrolling")) {
+    navbar.classList.add("scrolling");
+  } else if (currentScroll < 50) {
+    navbar.classList.remove("scrolling");
+  }
+  toggle.classList.toggle("show-menu");
+  menus.classList.toggle("show-menu");
+  menus.classList.add("fade-in");
+});
